2d208924fe7e4fad4a22ff163e4459f695bcea1a
BlackJeezuz <ruslan201010@gmail.com>
2018-05-27 17:09:14 +0300

    test pushed

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,33 @@
-var $            = require('gulp-load-plugins')();
 var gulp         = require('gulp'),
 browser      		 = require('browser-sync').create(),
 autoprefixer 		 = require('autoprefixer'),
 postcss      		 = require('gulp-postcss'),
 combiner     		 = require('stream-combiner2').obj,
 pngquant     		 = require('imagemin-pngquant'),
+imagemin 				 = require('gulp-imagemin'),
 iconfont     		 = require('gulp-iconfont'),
 consolidate  		 = require('gulp-consolidate'),
 svgstore     		 = require('gulp-svgstore'),
 rename       		 = require('gulp-rename'),
 svgmin       		 = require('gulp-svgmin'),
-rename           = require('gulp-rename'),
 inject       		 = require('gulp-inject'),
 notify					 = require('gulp-notify'),
 plumber 				 = require("gulp-plumber"),
 pug  						 = require('gulp-pug'),
-pugBEMify 			 = require('pug-bemify');
+pugBEMify 			 = require('pug-bemify'),
+browserify       = require('browserify'),
+watchify         = require('watchify'),
+source           = require('vinyl-source-stream'),
+sourcemaps 			 = require('gulp-sourcemaps'),
+sass             = require('gulp-sass');
 
 /*#Build plugins*/
 var csso = require('gulp-csso'),
 useref 	 = require('gulp-useref'),
 concat 	 = require('gulp-concat'),
 gulpif   = require('gulp-if'),
-uglify 	 = require('gulp-uglify');
+uglify 	 = require('gulp-uglify'),
+babel    = require('gulp-babel');
 
 var svgWatch            = './dist/img/svgstore/icons/*.svg';
 var svgIconsSource      = './sprite.svg';
@@ -45,18 +50,21 @@ gulp.task('svgstore', function () {
 	.pipe(gulp.dest(svgIconsDestination));
 });
 
+function errorFunction(error) {
+	return "Message to the notifier: " + error.message;
+}
+
 gulp.task('pug', function() {
-	return gulp.src('pug/*.pug')
-	.pipe(plumber())
-	.pipe(pug({
-		plugins : [pugBEMify()],
-		pretty: true
-	}))
-	.on("error", notify.onError(function(error) {
-		return "Message to the notifier: " + error.message;
-	}))
-	.pipe(gulp.dest('./'));
-});
+	return combiner(
+		gulp.src('pug/*.pug'),
+		plumber(),
+		pug({
+			plugins : [pugBEMify()],
+			pretty: true
+		}),
+		gulp.dest('./')
+		).on("error", notify.onError(errorFunction))
+	});
 
 gulp.task("build:icons", function() {
     return gulp.src(["./assets/icons/*.svg"]) //path to svg icons
@@ -84,38 +92,62 @@ gulp.task("build:icons", function() {
 gulp.task('img', function() {
 	return combiner(
 		gulp.src('./assets/img/**/*'),
-		$.imagemin({
+		imagemin({
 			interlaced: true,
 			progressice: true,
 			svgoPlugins: [{removeViewBox: false}],
 			une: [pngquant()]
 		}),
 		gulp.dest('./dist/img')
-		).on('error', $.notify.onError());
+		).on('error', notify.onError(errorFunction));
 });
 
 gulp.task('sass', function () {
 	return combiner(
 		gulp.src('assets/scss/**/*.scss'),
-		$.sourcemaps.init(),
-		$.sass({
+		sourcemaps.init(),
+		sass({
 			output_style: 'compressed'
 		}),
 		postcss([ autoprefixer({ browsers: ['last 3 version'] }) ]),
-		$.sourcemaps.write('.'),
+		sourcemaps.write('.'),
 		gulp.dest('./dist/css'),
 		browser.stream({match: '**/*.css'})
-		).on('error', $.notify.onError());
+		).on('error', notify.onError(errorFunction));
 });
 
 
 // Starts a BrowerSync instance
-gulp.task('serve', ['sass'], function(){
+gulp.task('serve', ['sass', 'babel'], function(){
 	browser.init({
 		server: {
 			baseDir: "./"
 		}
 	});
+});
+
+var sourceFile = './assets/common.js',
+destFolder = './dist/libs/',
+destFile = 'app.js';
+
+gulp.task('browserify', function() {
+
+	var b = browserify({
+		entries: ['./assets/common.js'],
+		cache: {},
+		packageCache: {},
+		plugin: [watchify]
+	});
+
+	b.on('update', rebundle);
+
+	function rebundle() {
+		return b.bundle()
+		.pipe(source(destFile))
+		.pipe(gulp.dest(destFolder));
+	}
+
+	return rebundle();
 });
 
 gulp.task('build:css', ['sass'], function() {
@@ -129,22 +161,30 @@ gulp.task('build:css', ['sass'], function() {
 	.pipe(gulp.dest('./dist/css/'));
 });
 
+gulp.task('babel', function() {
+	return combiner(
+		gulp.src('assets/js/*.js'),
+		babel({presets: ['es2015']}),
+		gulp.dest('dist/js/')
+	).on('error', notify.onError(errorFunction))
+})
 
-gulp.task('build:js', function() {
+gulp.task('build:js', ['babel'], function() {
 	return gulp.src([
-		'dist/libs/*.js' 
+		'dist/libs/**/*.js', 
+		'dist/js/*.js'
 		])
+	.pipe(sourcemaps.init())
 	.pipe(concat('libs.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'));
+	.pipe(gulp.dest('dist/libs/'));
 });
 
-gulp.task('build', ['pug', 'img', 'build:css', 'build:js']);
+gulp.task('build', ['img', 'build:css', 'build:js']);
 
-// Runs all of the above tasks and then waits for files to change
 gulp.task('default', ['serve'], function() {
 	gulp.watch('pug/**/*.pug', ['pug']);
 	gulp.watch(['assets/scss/**/*.scss'], ['sass']);
 	gulp.watch('./**/*.html').on('change', browser.reload);
-	gulp.watch('dist/libs/*.js').on('change', browser.reload);
+	gulp.watch(['assets/js/**/*.js'], ['babel']).on('change', browser.reload);
 });
