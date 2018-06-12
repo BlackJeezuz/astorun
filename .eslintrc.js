305cbf6d37f2712895727d6ed03f2081c8a614a0
BlackJeezuz <ruslan201010@gmail.com>
2018-06-12 17:48:55 +0300

    linter fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,28 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb',
-  rules: {
-    'linebreak-style': 0,
-    'global-require': 0,
-    'no-plusplus': 0,
-    'no-undef': 0,
-    'eol-last': 0,
-    'no-unused-vars': 0,
-    'react/prop-types': 0,
-    'no-trailing-spaces': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
   },
+  env: {
+    browser: true
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    'standart'
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  ],
+  // required to lint *.vue files
+  plugins: ['vue'],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'semi-quotes': true,
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 };
