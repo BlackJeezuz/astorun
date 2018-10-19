dcf74702694cb08f8b802ab0c1780e9f53e6820f
BlackJeezuz <ruslan201010@gmail.com>
2018-10-19 18:02:50 +0300

    shop push

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,21 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb',
-  rules: {
-    'linebreak-style': 0,
-    'global-require': 0,
-    'no-plusplus': 0,
-    'no-undef': 0,
-    'eol-last': 0,
-    'no-unused-vars': 0,
-    'react/prop-types': 0,
-    'no-trailing-spaces': 0,
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'semi': 0,
-    'react/no-did-mount-set-state': 0,
-    'comma-dangle': 0,
-    'space-before-function-paren': 0,
-    'arrow-parens': 0,
-    'react/forbid-prop-types': 0,
-    'arrow-body-style': 0,
-    'no-restricted-syntax': 0,
-    'no-underscore-dangle': 0,
-    "jsx-a11y/label-has-for": [ 2, {
-        "components": [ "Label" ],
-        "required": {
-            "every": [ "id" ]
-        },
-        "allowChildren": false
-    }]
-  }
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {}
 }
