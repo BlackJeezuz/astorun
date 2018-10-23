ddca45074cd58c09b5556170db577c74c90feec8
BlackJeezuz <ruslan201010@gmail.com>
2018-10-23 09:13:06 +0300

    src added

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -29,8 +29,7 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
-    },
-    publicPath: process.env.DEPLOY_ENV === 'GH_PAGES' ? '_nuxt' : '/_nuxt/'
+    }
   },
   modules: [
     ['nuxt-sass-resources-loader', '@/assets/styles/tools/tools.scss'],
